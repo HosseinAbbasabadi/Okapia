bc4f33764a7c9a37903ba89c0994ef6f91b0be80
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2019-06-12 12:54:57 +0430

    customer navigator moved to NavigatorViewComponent

---
+++ Okapia/Areas/Customer/ViewComponents/NavigatorViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Okapia.Areas.Customer.ViewComponents
+{
+    public class NavigatorViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Default");
+        }
+    }
+}
