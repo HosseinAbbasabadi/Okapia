e5bbc9258b60ae4f5746c178750a295b5eb32670
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2019-06-17 05:19:23 +0430

    notification view component implemented;

---
+++ Okapia/Areas/Administrator/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Okapia.Areas.Administrator.Models
+{
+    public class Notification
+    {
+        [Display(Name = "عنوان")]
+        public string Title { get; set; }
+
+        [Display(Name = "توضیحات")]
+        public string Description { get; set; }
+
+        [Display(Name = "لینک ارجاع")]
+        public string Link { get; set; }
+
+        [Display(Name = "از تاریخ")]
+        public DateTime FromDate { get; set; }
+
+        [Display(Name = "تا تاریخ")]
+        public DateTime ToDate { get; set; }
+        
+    }
+}
