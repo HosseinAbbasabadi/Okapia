3a9cb89f1e36dba1554c04df99062acb6a44e04f
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2019-06-19 13:32:41 +0430

    project created

---
+++ Services/Okapia_DataCollector/Okapia_DataCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Okapia_DataCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Site/Okapia/Helpers/Constants.cs
@@ -0,0 +1,32 @@
+﻿namespace Okapia.Helpers
+{
+    public static class Constants
+    {
+        public static Roles Roles { get; } = new Roles();
+        public static Cookies Cookies { get; } = new Cookies();
+        public static Names Names { get; } = new Names();
+    }
+
+    public class Roles
+    {
+        public string Administrator { get; } = "Administrator";
+        public string Customer { get; } = "Customer";
+        public string Job { get; } = "Job";
+        public string Club { get; } = "Club";
+    }
+
+    public class Cookies
+    {
+        public string AuthecticationCookieName { get; } = "Okapia_Authentication";
+        public string RoleCookieName { get; } = "Okapia_Role";
+        public string UsernameCookieName { get; } = "Okapia_Username";
+    }
+
+    public class Names
+    {
+        public string Ali { get; } = "Ali";
+        public string Hasan { get; } = "Hasan";
+        public string Hossein { get; } = "Hossein";
+        public string Mahdi { get; } = "Mahdi";
+    }
+}
