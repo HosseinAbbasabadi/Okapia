4b2fc319558495d615420c81e23fa28480c50bdf
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2019-09-26 12:15:59 +0330

    some bugs fixed

--- Site/Okapia.Query/Query/PageCategoryQuery.cs
+++ Site/Okapia.Query/Query/PageCategoryQuery.cs
@@ -17,7 +17,8 @@ namespace Okapia.Query.Query
 
         public List<PageCategoryItemViewModel> GetPageCategoriesForFooter()
         {
-            return _context.PageCategory.Where(x => x.PageCategoryIsDeleted == false && x.PageCategoryParentId == 0).Select(x => new PageCategoryItemViewModel
+            return _context.PageCategory.Where(x => x.PageCategoryIsDeleted == false && x.PageCategoryParentId == 0)
+                .Select(x => new PageCategoryItemViewModel
                 {
                     PageCategoryId = x.PageCategoryId,
                     PageCategorySlug = x.PageCategorySlug,
@@ -32,21 +33,52 @@ namespace Okapia.Query.Query
 
         public PageCategoryBlogViewModel GetPageCategoryForBlog(string categorySlug)
         {
-            var pageCategory = _context.PageCategory.Include(x => x.Pages).Where(x => x.PageCategoryIsDeleted == false)
-                .FirstOrDefault(x => x.PageCategorySlug == categorySlug);
-            if (pageCategory == null) return new PageCategoryBlogViewModel();
-            var getPagesForBlog = new PageCategoryBlogViewModel
+            if (categorySlug == "همه")
             {
-                PageCategoryId = pageCategory.PageCategoryId,
-                PageCategoryMetaTags = pageCategory.PageCategoryMetaTag,
-                PageCategoryMetaDescription = pageCategory.PageCategoryMetaDesccription,
-                PageCategoryCanonicalAddress = pageCategory.PageCanonicalAddress,
-                PageCategorySlug = pageCategory.PageCategorySlug,
-                PageCategoryName = pageCategory.PageCategoryName,
-                MetaTags = pageCategory.PageCategoryMetaTag.Split(",").ToList(),
-                Pages = MapPages(pageCategory.Pages.ToList())
-            };
-            return getPagesForBlog;
+                var pageCategory = new PageCategoryBlogViewModel
+                {
+                    MetaTags = new List<string> {"", "", ""},
+                    PageCategoryMetaDescription = "",
+                    PageCategorySlug = "همه",
+                    PageCategoryName = "همه",
+                    PageCategoryMetaTags = ""                    
+                };
+
+                pageCategory.Pages = _context.Page.Where(x => x.PageIsDeleted == false).Select(page =>
+                    new PageItemViewModel
+                    {
+                        PageId = page.PageId,
+                        PageSlug = page.PageSlug,
+                        PagePicture = page.PagePicture,
+                        PagePictureTitle = page.PagePictureTitle,
+                        PagePictureAlt = page.PagePictureAlt,
+                        PagePictureDescription = page.PagePictureDescription,
+                        PageCategory = page.PageCategory.PageCategoryName,
+                        PagePublishDate = page.PagePublishDate.ToFarsi(),
+                        PageTitle = page.PageTitle,
+                        PageSmallDescription = page.PageSmallDescription
+                    }).ToList();
+                return pageCategory;
+            }
+            else
+            {
+                var pageCategory = _context.PageCategory.Include(x => x.Pages)
+                    .Where(x => x.PageCategoryIsDeleted == false)
+                    .FirstOrDefault(x => x.PageCategorySlug == categorySlug);
+                if (pageCategory == null) return new PageCategoryBlogViewModel();
+                var getPagesForBlog = new PageCategoryBlogViewModel
+                {
+                    PageCategoryId = pageCategory.PageCategoryId,
+                    PageCategoryMetaTags = pageCategory.PageCategoryMetaTag,
+                    PageCategoryMetaDescription = pageCategory.PageCategoryMetaDesccription,
+                    PageCategoryCanonicalAddress = pageCategory.PageCanonicalAddress,
+                    PageCategorySlug = pageCategory.PageCategorySlug,
+                    PageCategoryName = pageCategory.PageCategoryName,
+                    MetaTags = pageCategory.PageCategoryMetaTag.Split(",").ToList(),
+                    Pages = MapPages(pageCategory.Pages.ToList())
+                };
+                return getPagesForBlog;
+            }
         }
 
         private static List<PageItemViewModel> MapPages(List<Page> pages)
--- Site/Okapia/ViewComponents/MobileCategoriesViewComponent.cs
+++ Site/Okapia/ViewComponents/MobileCategoriesViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Okapia.Application.Contracts;
 using Okapia.Domain.QueryContracts;
 
 namespace Okapia.ViewComponents
@@ -6,15 +7,17 @@ namespace Okapia.ViewComponents
     public class MobileCategoriesViewComponent : ViewComponent
     {
         private readonly ICategoryQuery _categoryQuery;
-
-        public MobileCategoriesViewComponent(ICategoryQuery categoryQuery)
+        private readonly ISettingApplication _settingApplication;
+        public MobileCategoriesViewComponent(ICategoryQuery categoryQuery, ISettingApplication settingApplication)
         {
             _categoryQuery = categoryQuery;
+            _settingApplication = settingApplication;
         }
 
         public IViewComponentResult Invoke(string pn)
         {
             var categories = _categoryQuery.GetCategoriesForMenu();
+            ViewData["tel"] = _settingApplication.GetSettingsForView().Tell1;
             ViewData["province"] = pn;
             return View("_MobileCategories", categories);
         }
